f95722946c4be42fdc0d6ab245724ea18f5d5304
Lauv <pussaaon@gmail.com>
2024-09-30 15:43:04 +0700

    init project

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,7 @@
 export default function Index() {
-    return (<div>Hello, My App</div>)
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-4">
+      <h1 className="text-4xl font-bold text-center flex-2">Have a Good Weather Day!</h1>
+    </main>
+  )
 }
