5c64617367160f644e2ad6827015b91b2e62f506
Lauv <pussaaon@gmail.com>
2024-09-30 16:09:10 +0700

    SelectedCities component

---
+++ src/app/_components/SelectedCities.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import moment from 'moment-timezone';
+import Link from 'next/link';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
+
+import { City, WeatherData } from '../_lib/models';
+import { useSelectedCities } from '../_context/CitiesContext';
+import { IoIosRemoveCircle } from "react-icons/io";
+import { getWeatherByCoords } from '../_services/apiWeather';
+import { openWeatherIconUrl } from '../_lib/constants';
+
+
+function SelectedCities () {
+
+	const { selectedCities, removeCity } = useSelectedCities();
+	const [isLoading, setIsLoading] = useState(true);	
+	const [cities, setCities] = useState<City[]>([]);
+
+	useEffect(() => {
+		const fetchCities = async () => {
+			const cities = await Promise.all(selectedCities.map(async (city: City) => {				
+				const weather: WeatherData = await getWeatherByCoords({ 
+					lat: city.center[1].toString(), 
+					lon: city.center[0].toString()
+				});
+				city.weather = weather;
+				return city;
+			}))
+			
+			setCities(cities);
+			setIsLoading(false);
+		}
+		fetchCities()
+	}, [selectedCities]);
+
+	if (isLoading) {
+		return <div className='text-gray-500 text-center'>Loading Today Weather. Anytime Now...</div>
+	}
+
+	if (selectedCities.length === 0) {
+		return <div className='text-gray-500 text-center'>- Search for a city to see the weather -</div>
+	}
+
+	const handleRemoveCity = (e: any, city: City) => {
+		e.stopPropagation();
+		e.preventDefault();
+		removeCity(city)
+	}
+
+	return (
+		<>
+			<ul className="w-full">
+				{cities && cities.map((city: City) => (
+					<li key={city.id}>				
+						<div 
+							className='flex gap-2 md:gap-4 p-4 border-b border-gray-300 items-center cursor-pointer'>
+							<p className='font-bold flex-1'>{city.place_name}</p>
+							<p>
+								{moment.utc().add(city.weather.timezone, 'seconds').format('YYYY-MM-DD HH:mm')}
+							</p>
+							<p>
+								{city.weather.weather[0].icon && 
+								<Image 
+									src={`${openWeatherIconUrl}${city.weather.weather[0].icon}.png`} 
+									alt={city.weather.weather[0].description} 										
+									width={48} 
+									height={48} />}
+							</p>
+							<p className='w-10 text-center'>{city.weather.main.temp.toFixed(0)}&deg;</p>
+							<p>
+								<IoIosRemoveCircle 
+									className='cursor-pointer text-gray-500'
+									onClick={(e) => { handleRemoveCity(e, city) }} />
+							</p>
+						</div>
+					</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+export default SelectedCities;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import SearchCities from "./_components/SearchCities";
+import SelectedCities from "./_components/SelectedCities";
 
 export default function Index() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-4">
       <h1 className="text-4xl font-bold text-center flex-2">Have a Good Weather Day!</h1>
       <SearchCities />
+      <SelectedCities />
     </main>
   )
 }
